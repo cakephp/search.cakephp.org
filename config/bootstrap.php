15d776b5f5f12b438020808d3ed4ea1bec5df9e0
Mark Story <mark@mark-story.com>
2015-09-03 23:00:21 -0400

    Clean up configuration files.
    Remove files that won't be used and update composer.json to contain
    actual dependencies for this app.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -176,11 +176,9 @@ Request::addDetector('tablet', function ($request) {
  * advanced ways of loading plugins
  *
  * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('Migrations'); //Loads a single plugin named Migrations
  *
  */
-
-Plugin::load('Migrations');
+Plugin::load('Cake/ElasticSearch', ['bootstrap' => true]);
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
